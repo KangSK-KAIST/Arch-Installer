c48faee73f1b4bdf54fc29f3e9a9203b329df002
KangSK-KAIST <steelyk7@gmail.com>
2020-12-07 15:47:27 +0900

    Added VirtualBox Installation

--- main.sh
+++ main.sh
@@ -4,16 +4,17 @@
 sudo pacman -Syu
 
 # Install basic programs
-sudo pacman -Sy base-devel git
+sudo pacman -Sy base-devel git go
 
 # Install yay for AUR
+# TODO: Do this only if yay is not installed
 cd $HOME/Downloads
 git clone https://aur.archlinux.org/yay-git.git
 cd yay-git
 makepkg -si
 
 # Test whether yay is well installed
-yay -Syu dropbox
+yay
 
 # Install LaTeX
 sudo pacman -Sy texlive-most texlive-langkorean texlive-games
