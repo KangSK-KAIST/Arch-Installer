9fd61c470dc88e8c5fc239c5a4d50a0f15fdfa85
KangSK-KAIST <steelyk7@gmail.com>
2020-12-07 15:54:59 +0900

    Added VirtualBox Installation - Fixed Bug

--- main.sh
+++ main.sh
@@ -16,6 +16,16 @@ makepkg -si
 # Test whether yay is well installed
 yay
 
+# Install VirtualBox
+sudo pacman -Sy virtualbox virtualbox-guest-iso
+sudo gpasswd -a $USER vboxusers
+sudo modprobe vboxdrv
+# Install VirtualBox ext modules
+yay -Sy virtualbox-ext-oracle
+sudo systemctl enable vboxweb.service
+sudo systemctl start vboxweb.service
+
+
 # Install LaTeX
 sudo pacman -Sy texlive-most texlive-langkorean texlive-games
 
